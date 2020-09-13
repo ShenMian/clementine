8e4473bd9d22c0c0e77fc43ec6c21e3662715ded
ShenMian <sms_school@outlook.com>
2020-09-13 11:06:10 +0800

    将 Input 类做为被观察者

--- clem/input.cpp
+++ clem/input.cpp
@@ -1,6 +1,6 @@
 // Copyright 2020 SMS
 // License(Apache-2.0)
-// è¾“å…¥
+// ÊäÈë
 
 #include "input.h"
 
--- clem/input.h
+++ clem/input.h
@@ -1,6 +1,6 @@
 // Copyright 2020 SMS
 // License(Apache-2.0)
-// 输入组件
+// ����
 
 #ifndef CLEM_INPUT_COMPONENT_H_
 #define CLEM_INPUT_COMPONENT_H_
@@ -9,8 +9,9 @@ typedef unsigned short ushort;
 
 #include <map>
 #include <vector>
+#include "observable.h"
 
-class Input
+class Input : public Observable
 {
 public:
 	virtual void update() = 0;
@@ -23,7 +24,7 @@ public:
 protected:
 	std::map<ushort, ushort> index;
 	std::vector<ushort>      events;
-	ushort                   interval = 150; // 重复响应持续相同按键的时间
+	ushort                   interval = 150; // 重复响应持续相同按键的时�?
 };
 
 #endif // CLEM_INPUT_COMPONENT_H_
