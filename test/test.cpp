20a347694c88beb47fc114465b7a8cdd30e97aa0
ShenMian <sms_school@outlook.com>
2020-09-20 16:19:18 +0800

    创建 单元测试

---
+++ test/test.cpp
@@ -0,0 +1,11 @@
+// Copyright 2020 SMS
+// License(Apache-2.0)
+
+#define BOOST_TEST_MODULE Clementine
+#include <boost/test/included/unit_test.hpp>
+
+#include <clem/clem.hpp>
+
+BOOST_AUTO_TEST_CASE(size)
+{
+}
