f02747edfae62edb50b88c1bd894582ec386e399
ShenMian <sms_school@outlook.com>
2020-09-25 05:33:00 +0800

    删除 Boost 头文件

--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,7 @@
 // Copyright 2020 SMS
 // License(Apache-2.0)
 
-#define BOOST_TEST_MODULE Clementine
-#include <boost/test/included/unit_test.hpp>
+
 #include <clem/clem.hpp>
 
 BOOST_AUTO_TEST_CASE(size)
