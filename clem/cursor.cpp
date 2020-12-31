fa3040da990a4bc07ac122e50960d6ae327f44b1
ShenMian <sms_school@outlook.com>
2020-12-31 13:18:55 +0800

    完善 Cursor

--- clem/cursor.cpp
+++ clem/cursor.cpp
@@ -3,21 +3,21 @@
 #include <stdio.h>
 #include <clem/type/point.h>
 
-void Cursor::moveTo(const Point& p) const
+void Cursor::moveTo(const Point& p)
 {
 		printf("\x1b[%d;%dH", (int)p.x, (int)p.y);
 }
 
-void Cursor::moveTo(ushort x, ushort y) const
+void Cursor::moveTo(ushort x, ushort y)
 {
 	printf("\x1b[%d;%dH", x, y);
 }
 
-void Cursor::setVisible(bool v) const
+void Cursor::setVisible(bool v)
 {
 	if(v)
-		;
+		printf("\e[?25h");
 	else
-		;
+		printf("\e[?25l");
 }
 
--- clem/cursor.h
+++ clem/cursor.h
@@ -5,17 +5,15 @@ class Point;
 class Cursor
 {
 public:
-	void moveTo(const Point& pos) const;
-	void moveTo(ushort x, ushort y) const;
+	static void moveTo(const Point& pos);
+	static void moveTo(ushort x, ushort y);
+  
+	static void setVisible(bool);
+};
 
+/*
 	static void moveUp(ushort n);
 	static void moveDown(ushort n);
 	static void moveRight(ushort n);
 	static void moveLeft(ushort n);
-  
-	void setVisible(bool) const;
-
-	static void show();
-  static void hide();
-};
-
+*/
