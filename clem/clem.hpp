18c240ba71c035f6d90a734faa9aab80ff7c7c97
ShenMian <sms_school@outlook.com>
2021-01-26 07:26:18 +0800

    完善 clem.hpp

--- clem/clem.hpp
+++ clem/clem.hpp
@@ -4,18 +4,19 @@
 #ifndef CLEM_CLEM_HPP_
 #define CLEM_CLEM_HPP_
 
+#include <clem/platform.h>
+
 #include <clem/director.h>
+#include <clem/scene.h>
+#include <clem/camera.h>
+#include <clem/factor.h>
 
 #include <clem/cursor.h>
 #include <clem/color.h>
-#include <clem/attribute.h>
 
-#include <clem/factor.h>
-
-#include <clem/scene.h>
-#include <clem/camera.h>
-
+/*
 #include <clem/observer.h>
 #include <clem/input.h>
+*/
 
 #endif // CLEM_CLEM_HPP_
