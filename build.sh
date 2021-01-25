e6b7c1e940f96c57ba9a050300a95e7f5f1ff867
ShenMian <sms_school@outlook.com>
2021-01-26 07:16:32 +0800

    完善 自动构建脚本

--- build.sh
+++ build.sh
@@ -2,12 +2,21 @@
 # Copyright 2020 SMS
 # License(Apache-2.0)
 
+# 切换到脚本所在目录
 cd "$( cd "$( dirname "$0"  )" && pwd  )"
+
+# 建立 build 目录, 用于构建
 if [ ! -d "./build" ]; then
 	mkdir build
 fi
 cd build
+
+# 下载子模块, 包含第三方库
+git submodule update --init --recursive
+
+# 构建
 cmake ..
 cmake --build .
-ctest
 
+# 运行单元测试
+ctest
