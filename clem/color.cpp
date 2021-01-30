2cfb62f097f8f760ae7f1923bb69d9516cf31b2f
ShenMian <sms_school@outlook.com>
2021-01-30 16:38:33 +0800

    完善 Color

--- clem/color.cpp
+++ clem/color.cpp
@@ -23,8 +23,32 @@ Color::Color(Fore f, Back b)
 
 	switch(f)
 	{
+	case Fore::black:
+		fore = "\x1b[30m";
+		break;
+
+	case Fore::red:
+		fore = "\x1b[31m";
+		break;
+
 	case Fore::green:
 		fore = "\x1b[32m";
+		break;
+
+	case Fore::yellow:
+		fore = "\x1b[33m";
+		break;
+
+	case Fore::blue:
+		fore = "\x1b[34m";
+		break;
+
+	case Fore::purple:
+		fore = "\x1b[35m";
+		break;
+
+	case Fore::cyan:
+		fore = "\x1b[36m";
 		break;
 
 	case Fore::white:
@@ -41,8 +65,36 @@ Color::Color(Fore f, Back b)
 		back = "\x1b[40m";
 		break;
 
+	case Back::red:
+		back = "\x1b[41m";
+		break;
+
+	case Back::green:
+		back = "\x1b[42m";
+		break;
+
+	case Back::yellow:
+		back = "\x1b[43m";
+		break;
+
+	case Back::blue:
+		back = "\x1b[44m";
+		break;
+
+	case Back::purple:
+		back = "\x1b[45m";
+		break;
+
+	case Back::cyan:
+		back = "\x1b[46m";
+		break;
+
+	case Back::white:
+		back = "\x1b[47m";
+		break;
+
 	default:
-		assert(false); // "无效背景颜色属性"
+		assert(false); // 无效背景颜色属性
 	}
 }
 
