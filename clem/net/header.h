b3e9ced237eaff18113c3eba8dda020446a88a71
ShenMian <sms_school@outlook.com>
2021-03-11 01:25:53 +0800

    完善 net/

--- clem/net/header.h
+++ clem/net/header.h
@@ -2,12 +2,14 @@
 #include <clem/platform.h>
 
 #ifdef OS_UNIX
-#include <sys/types.h>
-#include <sys/socket.h>
-typedef int socket_t;
+	#include <sys/types.h>
+	#include <sys/socket.h>
+	#include <netdb.h>
+	#include <arpa/inet.h>
+	typedef int socket_t;
 #endif // !OS_UNIX
 
 #ifdef OS_WIN
-#include <winsock2.h>
-typedef SOCKET socket_t;
+	#include <winsock2.h>
+	typedef SOCKET socket_t;
 #endif // !OS_WIN
