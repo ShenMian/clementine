c4403943f0e4e5d7dfeea3bb9c2af6ea1f02306a
ShenMian <sms_school@outlook.com>
2021-03-14 01:22:31 +0800

    添加 clem.h

---
+++ src/clem.h
@@ -0,0 +1,34 @@
+// Copyright 2021 SMS
+// License(Apache-2.0)
+
+#ifndef CLEM_CLEM_H_
+#define CLEM_CLEM_H_
+
+// Core
+#include "clem/camera.h"
+#include "clem/director.h"
+#include "clem/factor.h"
+#include "clem/scene.h"
+
+// Math
+#include "clem/math/point.h"
+#include "clem/math/vec2.h"
+
+// Physics
+#include "clem/physics/box_collider.h"
+#include "clem/physics/circle_collider.h"
+#include "clem/physics/collider.h"
+#include "clem/physics/rigidbody.h"
+
+// Input
+#include "clem/input/controller.h"
+#include "clem/input/keyboard.h"
+#include "clem/input/mouse.h"
+
+// Output
+#include "clem/color.h"
+#include "clem/cursor.h"
+#include "clem/frame_buffer.h"
+#include "clem/sprite.h"
+
+#endif // !CLEM_CLEM_H_
