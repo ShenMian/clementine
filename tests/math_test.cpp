b57294288e1276aef42877c7c522ac78f42ecc2f
ShenMian <sms_school@outlook.com>
2021-03-13 22:05:30 +0800

    添加 单元测试

---
+++ tests/math_test.cpp
@@ -0,0 +1,22 @@
+// Copyright 2021 SMS
+// License(Apache-2.0)
+
+#include "gtest/gtest.h"
+#include "clem/math/point.h"
+#include "clem/math/vec2.h"
+
+TEST(Point, distance)
+{
+	EXPECT_EQ(Point(0, 0).distance(Point(3, 4)), 5);
+}
+
+TEST(Vec2, length)
+{
+	EXPECT_EQ(Vec2(3, 4).length(), 5);
+}
+
+int main(int argc, char* argv[])
+{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
