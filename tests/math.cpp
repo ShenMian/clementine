d2e09bd106f8dc51b21cc5a9820befd893c042b9
ShenMian <sms_school@outlook.com>
2021-03-13 21:18:13 +0800

    添加 单元测试

--- tests/math.cpp
+++ tests/math.cpp
@@ -2,11 +2,15 @@
 // License(Apache-2.0)
 
 #include <gtest/gtest.h>
-#include <clem/clem.hpp>
+#include <clem/math/point.h>
+
+TEST(Point, distance)
+{
+    EXPECT_EQ(Point(0, 0).distance(Point(3, 4)), 5);
+}
 
 int main(int argc, char* argv[])
 {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
