9bbe034a65dc52e23a4c1703c0fa32423bd53693
ShenMian <sms_school@outlook.com>
2021-03-14 14:01:43 +0800

    完善 Spdlog

--- src/clem/log.cpp
+++ src/clem/log.cpp
@@ -3,36 +3,6 @@
 // 日志
 
 #include "log.h"
-#include <ctime>
-#include <cassert>
-#include <iostream>
+#include "spdlog/spdlog.h"
 
-using std::map;
-using std::string;
-using std::ofstream;
 
-ofstream Log::file;
-
-ofstream& Log::info(const string& s)
-{
-	file << "[INFO] " << s << std::endl;
-	return file;
-}
-
-std::ofstream& Log::warn(const string& s)
-{
-	file << "[WARN] " << s << std::endl;
-	return file;
-}
-
-std::ofstream& Log::error(const string& s)
-{
-	file << "[ERROR] " << s << std::endl;
-	return file;
-}
-
-std::ofstream& Log::fatal(const string& s)
-{
-	file << "[FATAL] " << s << std::endl;
-	return file;
-}
--- src/clem/log.h
+++ src/clem/log.h
@@ -1,35 +1,11 @@
 // Copyright 2021 SMS
 // License(Apache-2.0)
-// 日志
 
-#ifndef CLEM_LOGGER_H_
-#define CLEM_LOGGER_H_
+#ifndef CLEM_LOG_H_
+#define CLEM_LOG_H_
 
-#include <map>
-#include <string>
-#include <fstream>
-
-/// 日志
 class Log
 {
-public:
-	enum class Level;
-
-	static std::ofstream& info(const std::string&);
-	static std::ofstream& warn(const std::string&);
-	static std::ofstream& error(const std::string&);
-	static std::ofstream& fatal(const std::string&);
-
-private:
-	static std::ofstream file;
 };
 
-enum class Log::Level
-{
-	info,
-	warn,
-	error,
-	fatal
-};
-
-#endif // !CLEM_LOGGER_H_
+#endif // !CLEM_LOG_H_
