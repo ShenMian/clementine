85b09c002a77091da24d4900ba9782f558416c5f
ShenMian <sms_school@outlook.com>
2021-03-22 21:35:46 +0800

    添加 Cursor

---
+++ src/Clem/Renderer/Cursor.h
@@ -0,0 +1,38 @@
+﻿// Copyright 2021 SMS
+// License(Apache-2.0)
+
+#ifndef CLEM_RENDERER_CURSOR_H_
+#define CLEM_RENDERER_CURSOR_H_
+
+#include "Clem/Math/Vec2.h"
+
+/**
+ * @brief 光标.
+ */
+class Cursor
+{
+public:
+  /**
+   * @brief 移动光标到指定坐标
+   * 
+   * @param p 坐标
+   */
+  static void move(const Point& p);
+  
+  /**
+   * @brief 移动光标到指定坐标
+   * 
+   * @param x x坐标
+   * @param y y坐标
+   */
+	static void move(short x, short y);
+
+  /**
+   * @brief 设置光标可见性
+   * 
+   * @param v 可见性
+   */
+	static void setVisible(bool v);
+};
+
+#endif // !CLEM_RENDERER_CURSOR_H_
