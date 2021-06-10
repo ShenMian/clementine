4d18d4251ccd0f5fd4b2fc1e43ff994602dee790
ShenMian <sms_school@outlook.com>
2021-06-10 08:31:03 +0800

    添加 Audio/SoundBuffer

---
+++ Source/Clem/Audio/SoundBuffer.h
@@ -0,0 +1,56 @@
+// Copyright 2021 SMS
+// License(Apache-2.0)
+
+#pragma once
+
+#include <filesystem>
+
+namespace clem
+{
+/**
+ * @addtogroup Audio
+ * @{
+ */
+
+class SoundBuffer
+{
+public:
+	/**
+	 * @brief 默认构造函数.
+	 */
+	SoundBuffer();
+
+	~SoundBuffer();
+
+	/**
+	 * @brief 从文件加载声音.
+	 *
+	 * @param path 声音文件路径.
+	 */
+	bool loadFromFile(const std::filesystem::path& path);
+
+	const uint8_t* getSamples() const;
+
+	size_t getSampleCount() const;
+
+private:
+	std::vector<uint8_t> samples;
+	unsigned int         id; // OpenAL 缓冲区 ID
+
+	int                  frequency;
+	int                  format;
+
+	/**
+	 * 从 WAV 文件载入声音.
+	 *
+	 * @param path 文件路径.
+	 */
+	bool loadWavFile(const std::filesystem::path& path);
+};
+
+/**
+* end of Audio group
+* @}
+*/
+
+} // namespace clem
