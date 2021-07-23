c3deab49640723ee8b8649c53c8ffefd411d61b8
ShenMian <sms_school@outlook.com>
2021-07-24 00:22:02 +0800

    添加第三方库 ImGui, 清理第三方库

---
+++ Thirdparty/imgui/premake5.lua
@@ -0,0 +1,36 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "On"
+	
+	targetdir("%{wks.location}/build/" .. outputdir .. "/%{prj.name}/lib")
+	objdir("%{wks.location}/build/" .. outputdir .. "/%{prj.name}/obj")
+
+	files {
+    "include/**.h",
+    "src/**.cpp",
+    "backends/**.h",
+    "backends/imgui_impl_glfw.cpp",
+    "backends/imgui_impl_opengl3.cpp",
+
+    "premake5.lua"}
+
+	includedirs {
+        "include/imgui",
+        "%{thirdparty.glad}/include",
+        "%{thirdparty.glfw}/include"}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "system:linux"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
