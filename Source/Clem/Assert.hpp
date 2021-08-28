01a097a4be209c4168bbb35db7cca2575264f7a7
ShenMian <sms_school@outlook.com>
2021-08-28 18:17:45 +0800

    完善 Assert

--- Source/Clem/Assert.hpp
+++ Source/Clem/Assert.hpp
@@ -17,24 +17,28 @@ public:
 	 * @brief 断言.
 	 * 表达式值为假时断言失败, 报告消息并结束程序.
 	 *
-	 * @param expression 要测试的表达式.
-	 * @param message 提示消息.
-	 * @param location 代码定位.
+	 * @param expr 要测试的表达式.
+	 * @param msg  提示消息.
+	 * @param loc  代码定位.
 	 */
-    static void isTrue(bool expression, std::string_view message = "", const std::source_location& location = std::source_location::current());
+    static void isTrue(bool expr, std::string_view msg = "", const std::source_location& loc = std::source_location::current());
 
     /**
 	 * @brief 断言.
 	 * 表达式值为真时断言失败, 报告消息并结束程序.
 	 *
-	 * @param expression 要测试的表达式.
-	 * @param message 提示消息.
-	 * @param location 代码定位.
+	 * @param expr 要测试的表达式.
+	 * @param msg  提示消息.
+	 * @param loc  代码定位.
 	 */
-    static void isFalse(bool expression, std::string_view message = "", const std::source_location& location = std::source_location::current());
+    static void isFalse(bool expr, std::string_view msg = "", const std::source_location& loc = std::source_location::current());
 };
 
 // 仅限影响性能的测试使用
-#define CLEM_ASSERT(expression, message) Assert::isTrue(expression, message)
+#if 1
+#    define CLEM_ASSERT(expr, msg) Assert::isTrue(expr, msg)
+#else
+#    define CLEM_ASSERT(expr, msg)
+#endif
 
 } // namespace clem
