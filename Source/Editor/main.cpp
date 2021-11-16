af02713d20ed676a403c2b3c766593702618eea3
ShenMian <sms_school@outlook.com>
2021-11-16 23:52:01 +0800

    Add GamerServices

---
+++ Source/Editor/main.cpp
@@ -0,0 +1,27 @@
+#include <Core/Application.h>
+#include <GamerServices.h>
+
+using namespace clem;
+
+int main()
+{
+    GamerServices::init();
+
+    /*
+    Application::Config config = {
+        .display = {
+            .resolution = {100, 100},
+        }
+    };
+
+    auto& app = Application::get();
+    app.setConfig(config);
+
+    auto& win = app.getWindow();
+    win.setVisible(true);
+    */
+
+    GamerServices::deinit();
+
+    return 0;
+}
---
+++ Source/Engine/GamerServices.h
@@ -0,0 +1,11 @@
+// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+class GamerServices
+{
+public:
+    static void update();
+
+    static void init();
+    static void deinit();
+};
