134b78342e3b2d90e662a3d7f5edfffb445d6b44
ShenMian <sms_school@outlook.com>
2021-12-05 19:43:14 +0800

    Add Lobby

---
+++ Source/Engine/GamerServices/Lobby.cpp
@@ -0,0 +1,4 @@
+// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#include "Lobby.h"
---
+++ Source/Engine/GamerServices/Lobby.h
@@ -0,0 +1,15 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#pragma once
+
+#include <string>
+
+class Lobby
+{
+public:
+	static void queryAsync();
+
+	static void setStringFilter(const std::string& str);
+	static void setCountFilter(int count);
+};
