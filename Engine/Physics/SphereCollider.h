90c41abf90342b7ff6e43257dd66802f7ff8fb1b
ShenMian <70068015+ShenMian@users.noreply.github.com>
2022-04-09 02:10:36 +0800

    Add SphereCollider

---
+++ Engine/Physics/SphereCollider.h
@@ -0,0 +1,26 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#pragma once
+
+#include "Collider.h"
+
+namespace phys
+{
+
+class SphereCollider : public Collider
+{
+public:
+	bool intersects(const SphereCollider& other) const;
+
+	bool contains(const SphereCollider& other) const;
+
+	bool contains(const Vector3& point) const;
+
+	Vector3 center;
+	float radius;
+};
+
+}
+
+#include "SphereCollider.inl"
