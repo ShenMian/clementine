762c948d730d99333e03df77d93abc355fa41625
ShenMian <70068015+ShenMian@users.noreply.github.com>
2022-04-10 20:25:16 +0800

    Update Alogrithm.hpp

---
+++ Engine/Physics/Algorithm.hpp
@@ -0,0 +1,27 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#pragma once
+
+#include "BoxCollider.h"
+#include "SphereCollider.h"
+
+namespace phys
+{
+
+namespace algo
+{
+
+bool intersects(const BoxCollider& box, const SphereCollider& sphere)
+{
+	return false;
+}
+
+bool intersects(const SphereCollider& sphere, const BoxCollider& box)
+{
+	return intersects(box, sphere);
+}
+
+}
+
+}
