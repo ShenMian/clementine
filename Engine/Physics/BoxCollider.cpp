316525ed17756fe0e0384ce255301ff2f260411b
ShenMian <70068015+ShenMian@users.noreply.github.com>
2022-04-10 20:24:47 +0800

    Update Collider

---
+++ Engine/Physics/BoxCollider.cpp
@@ -0,0 +1,46 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#include "Algorithm.hpp"
+
+namespace phys
+{
+
+inline bool BoxCollider::intersects(const Collider& other) const
+{
+	return other.intersects(*this);
+}
+
+inline bool BoxCollider::intersects(const BoxCollider& other) const
+{
+	return contains(other.getMin()) || contains(other.getMax());
+}
+
+inline bool BoxCollider::intersects(const SphereCollider& other) const
+{
+	return algo::intersects(*this, other);
+}
+
+inline bool BoxCollider::contains(const BoxCollider& other) const
+{
+	return contains(other.getMin()) && contains(other.getMax());
+}
+
+inline bool BoxCollider::contains(const Vector3& point) const
+{
+	const auto max = getMax();
+	const auto min = getMin();
+	return min.x <= point.x && point.x <= max.x && min.y <= point.y && point.y <= max.y;
+}
+
+inline Vector3 BoxCollider::getMin() const
+{
+	return center - halfExtent;
+}
+
+inline Vector3 BoxCollider::getMax() const
+{
+	return center + halfExtent;
+}
+
+}
---
+++ Engine/Physics/SphereCollider.cpp
@@ -0,0 +1,36 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#include "Algorithm.hpp"
+
+namespace phys
+{
+
+inline bool SphereCollider::intersects(const Collider& other) const
+{
+	return other.intersects(*this);
+}
+
+inline bool SphereCollider::intersects(const SphereCollider& other) const
+{
+	const auto disSq = (center - other.center).normSq();
+	const auto minDisSq = (radius + other.radius) * (radius + other.radius);
+	return disSq <= minDisSq;
+}
+
+inline bool SphereCollider::intersects(const BoxCollider& other) const
+{
+	return algo::intersects(*this, other);
+}
+
+inline bool SphereCollider::contains(const SphereCollider& other) const
+{
+	return (center - other.center).norm() + other.radius < radius;
+}
+
+inline bool SphereCollider::contains(const Vector3& point) const
+{
+	return (center - point).normSq() <= radius * radius;
+}
+
+}
