a23b6dc8d0a4be46466a0d1b6c3cf1b2fa0ab7f2
ShenMian <70068015+ShenMian@users.noreply.github.com>
2022-04-17 00:10:28 +0800

    Rename 'Audio' to 'Device'

---
+++ Engine/Audio/Device.h
@@ -0,0 +1,37 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+class ALCdevice;
+
+namespace audio
+{
+
+/**
+ * @brief 音频设备.
+ */
+class Device
+{
+public:
+	Device(ALCdevice* handle);
+	virtual ~Device();
+
+	std::string_view getName() const;
+
+	int getSampleRate() const;
+
+	static void init();
+	static void deinit();
+
+private:
+	ALCdevice* handle;
+
+	inline static std::vector<Device> outputDevices;
+	inline static std::vector<Device> inputDevices;
+};
+
+}
