66f7ffff7f4153e3c168a76badcb5cb988405264
ShenMian <70068015+ShenMian@users.noreply.github.com>
2022-04-24 14:46:48 +0800

    Fix Emitter

--- Engine/Event/Emitter.hpp
+++ Engine/Event/Emitter.hpp
@@ -18,30 +18,47 @@ public:
 	/**
 	 * @brief 发射事件.
 	 *
-	 * @param name 事件名称.
-	 * @param args 传递给回调函数的参数.
+	 * @param event 事件名称.
+	 * @param args  传递给回调函数的参数.
 	 */
 	template <typename... Args>
-	void emit(const std::string& name, Args... args);
+	void emit(const std::string& event, Args... args);
 
 	/**
 	 * @brief 添加事件回调.
 	 *
-	 * @param name     事件名称.
+	 * @param event    事件名称.
 	 * @param callback 回调函数.
 	 */
 	template <typename Callback>
-	void add(const std::string& name, Callback callback);
+	void on(const std::string& event, Callback callback);
+
+	/**
+	 * @brief 添加一次性的事件回调.
+	 *
+	 * 回调只会在在事件触发后被执行一次.
+	 *
+	 * @param event    事件名称.
+	 * @param callback 回调函数.
+	 */
+	template <typename Callback>
+	void once(const std::string& event, Callback callback);
 
 	/**
 	 * @brief 移除事件回调.
 	 *
-	 * @param name 事件名称.
+	 * @param event 事件名称.
 	 */
-	void remove(const std::string& name);
+	void remove(const std::string& event);
+
+	/**
+	 * @brief 移除全部事件回调.
+	 */
+	void clear();
 
 private:
 	std::unordered_map<std::string, void*> listeners;
+	std::unordered_map<std::string, void*> onceListeners;
 };
 
 }
