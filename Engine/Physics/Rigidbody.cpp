a28b2798c64e38eb6ab439e2d01d652cc1260295
ShenMian <70068015+ShenMian@users.noreply.github.com>
2022-04-24 14:48:54 +0800

    Update Rigidbody

---
+++ Engine/Physics/Rigidbody.cpp
@@ -0,0 +1,43 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#include "Rigidbody.h"
+#include <numeric>
+
+namespace phys
+{
+
+void Rigidbody::mass(float m)
+{
+	assert(mass_ > std::numeric_limits<float>::epsilon());
+	mass_ = m;
+	invMass_ = 1.f / mass_;
+}
+
+float Rigidbody::mass() const
+{
+	return mass_;
+}
+
+float Rigidbody::invMass() const
+{
+	return invMass_;
+}
+
+void Rigidbody::inertia(float i)
+{
+	inertia_ = i;
+	invInertia_ = 1.f / inertia_;
+}
+
+float Rigidbody::inertia() const
+{
+	return inertia_;
+}
+
+float Rigidbody::invInertia() const
+{
+	return invInertia_;
+}
+
+}
