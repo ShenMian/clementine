5b06d2fcf3f25f2a459a6dc571e080f3c033f52d
ShenMian <70068015+ShenMian@users.noreply.github.com>
2022-04-24 14:48:54 +0800

    Update Rigidbody

--- Engine/Physics/Rigidbody.h
+++ Engine/Physics/Rigidbody.h
@@ -3,12 +3,47 @@
 
 #pragma once
 
+#include <Math/Math.hpp>
+
 namespace phys
 {
 
 class Rigidbody
 {
-	float mass = 0.f;
+public:
+	enum class Type
+	{
+		Static,
+		Kinematic,
+		Dynamic
+	};
+
+	Type type = Type::Static;
+
+	Vector3 linearVelocity;
+	float angularVelocity = 0.f;
+
+	float linearDamping = 0.f;
+	float angularDamping = 0.f;
+
+	bool awake = true;
+
+	float gravityScale = 0.f;
+
+	Vector3 force;
+	float torque;
+
+	void mass(float m);
+	float mass() const;
+	float invMass() const;
+
+	void inertia(float i);
+	float inertia() const;
+	float invInertia() const;
+
+private:
+	float mass_ = 1.f, invMass_ = mass_ / 1.f;
+	float inertia_ = 1.f, invInertia_ = inertia_ / 1.f;
 };
 
 struct Material
