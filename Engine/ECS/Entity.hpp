097757da09a34598a6668adb1d5960ffa047f1a8
ShenMian <70068015+ShenMian@users.noreply.github.com>
2022-05-17 14:58:00 +0800

    Add Entity.hpp

---
+++ Engine/ECS/Entity.hpp
@@ -0,0 +1,52 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#pragma once
+
+#include <cstdint>
+
+namespace ecs
+{
+
+class Registry;
+
+/**
+ * @brief 实体.
+ */
+class Entity
+{
+public:
+	using id_type = uint32_t;
+	using version_type = uint32_t;
+
+	Entity() = default;
+
+	constexpr Entity(id_type id, version_type ver, Registry& reg)
+		: id_(id), version_(ver), registry(reg)
+	{
+	}
+
+	constexpr auto id() const noexcept
+	{
+		return id_;
+	}
+
+	constexpr auto version() const noexcept
+	{
+		return version_;
+	}
+
+	bool operator==(const Entity& rhs) const
+	{
+		return id_ == rhs.id_ && version_ == rhs.version_ && &registry == &rhs.registry;
+	};
+
+private:
+	id_type id_;
+	version_type version_;
+	Registry& registry;
+
+	friend class Registry;
+};
+
+}
