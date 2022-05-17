c64855aa4410f9bd3557cec3ab810da4391d44b8
ShenMian <70068015+ShenMian@users.noreply.github.com>
2022-05-17 14:58:08 +0800

    Add Array.hpp

---
+++ Engine/ECS/Array.hpp
@@ -0,0 +1,53 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#pragma once
+
+#include <unordered_map>
+#include <vector>
+
+namespace ecs
+{
+
+class Entity;
+
+/**
+ * @brief 无缝数组.
+ */
+template<typename T>
+class Array
+{
+public:
+	void add(const Entity& entity, const T& component)
+	{
+		if(!index_.contains(entity))
+			index_.insert({entity, index_.size()});
+		const auto index = index[entity];
+		if(data_.size() <= index)
+			data_.resize(index);
+		data_[index] = component;
+	}
+
+	void remove(const Entity& entity) noexcept
+	{
+		const auto index = index[entity];
+		index_.erase(entity);
+		if(index == data_.size() - 1)
+		{
+			data_.pop_back();
+			return;
+		}
+		std::swap(data_[index], data_.back());
+	}
+
+	auto size() const noexcept
+	{
+		return index_.size();
+	}
+
+private:
+	std::vector<T> data_;
+	std::unordered_map<Entity, size_t> index_;
+};
+
+}
