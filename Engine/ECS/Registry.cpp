ef7e60432007b2cfc0eb9bbd5463fb84a131108f
ShenMian <70068015+ShenMian@users.noreply.github.com>
2022-05-17 14:58:35 +0800

    Add Registry

---
+++ Engine/ECS/Registry.cpp
@@ -0,0 +1,44 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#include "Registry.h"
+#include <cassert>
+
+namespace ecs
+{
+
+Entity Registry::create()
+{
+	return entities_[request()];
+}
+
+void Registry::destroy(const Entity& entity)
+{
+	recycle(entity.id());
+}
+
+bool Registry::valid(const Entity& entity) const noexcept
+{
+	return entity.id() < entities_.size() && entity == entities_[entity.id()];
+}
+
+Entity::id_type Registry::request()
+{
+	if(freeIds_.empty())
+	{
+		const auto id = entities_.size();
+		entities_.emplace_back(static_cast<Entity::id_type>(id), 0, *this);
+		return id;
+	}
+	const auto id = freeIds_.back();
+	freeIds_.pop_back();
+	return id;
+}
+
+void Registry::recycle(Entity::id_type id)
+{
+	freeIds_.push_back(id);
+	entities_[id].version_++;
+}
+
+}
---
+++ Engine/ECS/Registry.h
@@ -0,0 +1,49 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#pragma once
+
+#include <vector>
+#include <numeric>
+#include "Archetype.hpp"
+#include "Entity.hpp"
+
+namespace ecs
+{
+
+constexpr size_t max_entity_size = 5000;
+
+/**
+ * @brief 实体管理器.
+ */
+class Registry
+{
+public:
+	/**
+	 * @brief 创建实体.
+	 */
+	Entity create();
+
+	/**
+	 * @brief 销毁实体.
+	 *
+	 * @param entity 要销毁的实体.
+	 */
+	void destroy(const Entity& entity);
+
+	/**
+	 * @brief 判断实体是否有效.
+	 */
+	bool valid(const Entity& entity) const noexcept;
+
+private:
+	Entity::id_type request();
+	void recycle(Entity::id_type id);
+
+	std::vector<Entity> entities_;
+	std::vector<Entity::id_type> freeIds_;
+};
+
+static_assert(std::numeric_limits<Entity::id_type>::max() < std::numeric_limits<size_t>::max());
+
+}
