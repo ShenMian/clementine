cb2c2ae37083089abaa51a4a33043233af44d896
ShenMian <70068015+ShenMian@users.noreply.github.com>
2022-05-17 14:58:26 +0800

    Add View.hpp

---
+++ Engine/ECS/View.hpp
@@ -0,0 +1,62 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#pragma once
+
+#include <functional>
+
+namespace ecs
+{
+
+class Entity;
+
+template<typename... Ts>
+class Iterator
+{
+public:
+	std::pair<Entity&, std::tuple<Ts&...>> operator*()
+	{
+		return std::tie();
+	}
+
+	bool operator!=(const Iterator& rhs) const
+	{
+	}
+
+	Iterator& operator++()
+	{
+	}
+
+private:
+};
+
+template<typename... Ts>
+class View
+{
+public:
+	using iterator = Iterator<Ts...>;
+
+	void each(std::function<void(Entity&, Ts&...)> func)
+	{
+	}
+
+	iterator begin()
+	{
+	}
+
+	iterator end()
+	{
+	}
+
+	const iterator begin() const
+	{
+	}
+
+	const iterator end() const
+	{
+	}
+
+private:
+};
+
+}
