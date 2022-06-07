1cb33e50b1148d27660b67555ad9116dea7043ab
ShenMian <70068015+ShenMian@users.noreply.github.com>
2022-06-07 17:03:55 +0800

    Add CapsuleCollider

---
+++ Engine/Physics/CapsuleCollider.cpp
@@ -0,0 +1,35 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#include "CapsuleCollider.h"
+#include "Algorithm.hpp"
+
+namespace phys
+{
+
+CapsuleCollider::CapsuleCollider(const Vector3& center, float radius, float height)
+	: center(center), radius(radius), height(height)
+{
+}
+
+bool CapsuleCollider::intersects(const Collider& other) const
+{
+	return other.intersects(*this);
+}
+
+bool CapsuleCollider::intersects(const SphereCollider& other) const
+{
+	return false;
+}
+
+bool CapsuleCollider::intersects(const BoxCollider& other) const
+{
+	return false;
+}
+
+bool CapsuleCollider::contains(const Vector3& point) const
+{
+	return false;
+}
+
+}
---
+++ Engine/Physics/CapsuleCollider.h
@@ -0,0 +1,31 @@
+﻿// Copyright 2021 ShenMian
+// License(Apache-2.0)
+
+#pragma once
+
+#include "Collider.h"
+#include <Math/Math.hpp>
+
+namespace phys
+{
+
+class SphereCollider;
+class BoxCollider;
+
+class CapsuleCollider : public Collider
+{
+public:
+	CapsuleCollider(const Vector3& center, float radius, float height);
+
+	bool intersects(const Collider& other) const;
+	bool intersects(const SphereCollider& other) const;
+	bool intersects(const BoxCollider& other) const;
+
+	bool contains(const Vector3& point) const;
+
+	Vector3 center;
+	float radius;
+	float height;
+};
+
+}
