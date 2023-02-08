dee853050fe2e00805e409272bca8c91e28afc91
ShenMian <sms_school@outlook.com>
2023-02-09 00:43:02 +0800

    refactor: update check.hpp

--- src/engine/core/check.hpp
+++ src/engine/core/check.hpp
@@ -1,60 +1,87 @@
-// Copyright 2022 SMS
+// Copyright 2023 ShenMian
 // License(Apache-2.0)
 
 #pragma once
 
-#include "platform.h"
 #include <cstdio>
 #include <string_view>
-#include <type_traits>
+#include <exception>
 // #include <source_location>
+// TODO: 等待 apple-clang 支持 std::source_location. 
 
-#if TARGET_COMPILER == COMPILER_MSVC
-	#define breakpoint() __debugbreak()
+#if _MSC_VER
+#define breakpoint() __debugbreak()
 #else
-	#define breakpoint() __builtin_trap()
+#define breakpoint() __builtin_trap()
 #endif
 
 namespace core
 {
 
-inline constexpr void check(bool condition)
+/**
+ * @brief 断言.
+ *
+ * @param condition 条件.
+ */
+constexpr void check(bool condition)
 {
 	if(condition)
 		return;
-	breakpoint();
+
+	breakpoint(); // debug
+	// terminate(); // release
 }
 
-inline constexpr void check(bool condition, std::string_view message)
+/**
+ * @brief 断言.
+ *
+ * @param condition 条件.
+ * @param message   描述.
+ */
+constexpr void check(bool condition, std::string_view message)
 {
 	if(condition)
 		return;
+
 	std::puts(message.data());
-	breakpoint();
+	breakpoint(); // debug
+	// terminate(); // release
 }
-
-// TODO: clang 暂时不支持 std::source_location.
+ 
+/**
+ * @brief 断言.
+ *
+ * @param condition 条件.
+ */
 /*
-inline void check(bool condition, const std::source_location& loc =
-std::source_location::current())
+inline void check(bool condition, const std::source_location& loc = std::source_location::current())
 {
-        if(condition)
-                return;
+	if(condition)
+		return;
 
-        std::printf("Assertion failed %s:%s(%u:%u)\n",
-                loc.file_name(), loc.function_name(), loc.line(), loc.column());
-        breakpoint();
+	std::printf("Assertion failed %s:%s(%u:%u)\n", loc.file_name(), loc.function_name(), loc.line(), loc.column());
+	breakpoint(); // debug
+	// terminate(); // release
 }
+*/
 
-inline void check(bool condition, std::string_view message, const
-std::source_location& loc = std::source_location::current())
+/**
+ * @brief 断言.
+ *
+ * @param condition 条件.
+ * @param message   描述.
+ */
+/*
+inline void check(bool condition, std::string_view message,
+                  const std::source_location& loc = std::source_location::current())
 {
-        if(condition)
-                return;
+	if(condition)
+		return;
 
-        std::printf("Assertion failed %s:%s(%u:%u): %s\n",
-                loc.file_name(), loc.function_name(), loc.line(), loc.column(),
-message.data()); breakpoint();
+	std::printf("Assertion failed %s:%s(%u:%u): %s\n", loc.file_name(), loc.function_name(), loc.line(), loc.column(),
+	            message.data());
+	breakpoint(); // debug
+	// terminate(); // release
 }
 */
 
