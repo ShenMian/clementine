0683a4649f285f50c7941891dd90c9f9b16b8f5f
ShenMian <sms_school@outlook.com>
2023-02-17 00:08:46 +0800

    feat: add benchmark

--- tests/benchmark.cpp
+++ tests/benchmark.cpp
@@ -1,24 +1,146 @@
 // Copyright 2023 ShenMian
 // License(Apache-2.0)
 
+#include "ecs/component.hpp"
+#include "ecs/manager.hpp"
 #include "ecs/packed_array.hpp"
+#include "ecs/view.hpp"
 #include <doctest/doctest.h>
 #include <nanobench.h>
+
+#include <iostream>
+
+#include <cmath>
+#include <ranges>
 
 using namespace ankerl;
 
 TEST_SUITE_BEGIN("benchmark");
 
-TEST_CASE("PackedArray" * doctest::skip(true))
+struct AComponent : public ecs::Component
 {
-	ecs::PackedArray<int> array;
-	for(size_t i = 0; i < 1000; i++)
-		array[i] = i;
+	DECLARE_TYPE
 
-	nanobench::Bench().minEpochIterations(100).run("packed array", [&] {
-		for(size_t i = 0; i < 1000; i++)
-			nanobench::doNotOptimizeAway(array[i]);
-	});
+	uint32_t value;
+};
+
+struct BComponent : public ecs::Component
+{
+	DECLARE_TYPE
+
+	uint32_t value;
+};
+
+TEST_CASE("PackedArray add component" * doctest::skip(true))
+{
+	nanobench::Bench bench;
+	// TODO: std::views::iota(4) | std::views::transform([](auto i) { return std::pow(2, i); }) | std::views::take(12);
+	for(const auto size : {16, 64, 256, 1 * 1000, 4 * 1000, 16 * 1000, 65 * 1000, 262 * 1000, 1000 * 1000})
+	{
+		ecs::PackedArray<AComponent> array;
+		bench.complexityN(size).run("create " + std::to_string(size) + " elements in the packed array", [&] {
+			for(size_t i = 0; i < size; i++)
+				array[i].value = i;
+		});
+	}
+	// std::cout << bench.complexityBigO() << std::endl;
+}
+
+TEST_CASE("PackedArray get component" * doctest::skip(true))
+{
+	nanobench::Bench bench;
+	for(const auto size : {16, 64, 256, 1 * 1000, 4 * 1000, 16 * 1000, 65 * 1000, 262 * 1000, 1000 * 1000})
+	{
+		ecs::PackedArray<AComponent> array;
+		for(size_t i = 0; i < size; i++)
+			array[i].value = i;
+		bench.complexityN(size).run("get " + std::to_string(size) + " elements in the packed array", [&] {
+			for(size_t i = 0; i < size; i++)
+				nanobench::doNotOptimizeAway(array[i]);
+		});
+	}
+	// std::cout << bench.complexityBigO() << std::endl;
+}
+
+TEST_CASE("PackedArray remove component" * doctest::skip(true))
+{
+	nanobench::Bench bench;
+	for(const auto size : {16, 64, 256, 1 * 1000, 4 * 1000, 16 * 1000, 65 * 1000, 262 * 1000, 1000 * 1000})
+	{
+		ecs::PackedArray<AComponent> array;
+		for(size_t i = 0; i < size; i++)
+			array[i].value = i;
+		bench.complexityN(size).run("remove " + std::to_string(size) + " elements in the packed array", [&] {
+			auto arr = array;
+			for(size_t i = 0; i < size; i++)
+				arr.remove(i);
+			CHECK(arr.size() == 0);
+		});
+	}
+	// std::cout << bench.complexityBigO() << std::endl;
+}
+
+TEST_CASE("create entities" * doctest::skip(true))
+{
+	nanobench::Bench bench;
+	for(const auto size : {16, 64, 256, 1 * 1000, 4 * 1000, 16 * 1000, 65 * 1000, 262 * 1000, 1000 * 1000})
+	{
+		bench.complexityN(size).run("create " + std::to_string(size) + " entities with 2 components", [&] {
+			ecs::Manager manager;
+			for(size_t i = 0; i < size; i++)
+			{
+				auto entity = manager.create();
+				manager.add_component<AComponent>(entity);
+				manager.add_component<BComponent>(entity);
+			}
+		});
+	}
+	// std::cout << bench.complexityBigO() << std::endl;
+}
+
+TEST_CASE("destroy entities" * doctest::skip(true))
+{
+	nanobench::Bench bench;
+	for(const auto size : {16, 64, 256, 1 * 1000, 4 * 1000, 16 * 1000, 65 * 1000, 262 * 1000, 1000 * 1000})
+	{
+		ecs::Manager manager;
+		for(size_t i = 0; i < size; i++)
+		{
+			auto entity = manager.create();
+			manager.add_component<AComponent>(entity);
+			manager.add_component<BComponent>(entity);
+		}
+		bench.complexityN(size).run("destroy " + std::to_string(size) + " entities with 2 components", [&] {
+			for(size_t i = 0; i < size; i++)
+				manager.destroy(ecs::Entity(i, 1));
+		});
+	}
+	// std::cout << bench.complexityBigO() << std::endl;
+}
+
+TEST_CASE("destroy entities" * doctest::skip(true))
+{
+	nanobench::Bench bench;
+	for(const auto size : {16, 64, 256, 1 * 1000, 4 * 1000, 16 * 1000, 65 * 1000, 262 * 1000, 1000 * 1000})
+	{
+		ecs::Manager manager;
+		for(size_t i = 0; i < size; i++)
+		{
+			auto entity = manager.create();
+			manager.add_component<AComponent>(entity);
+			manager.add_component<BComponent>(entity);
+		}
+		manager.add_group<AComponent, BComponent>();
+		bench.complexityN(size).run("unpack two components in " + std::to_string(size) + " entities", [&] {
+			auto view = ecs::View<AComponent, BComponent>(manager.get_group<AComponent, BComponent>(), manager);
+			for(auto [e, a, b] : view)
+			{
+				nanobench::doNotOptimizeAway(a);
+				nanobench::doNotOptimizeAway(b);
+			}
+		});
+	}
+	// std::cout << bench.complexityBigO() << std::endl;
 }
 
 TEST_SUITE_END;
