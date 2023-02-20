49d3209c38b50745f60a7a365145a998d45c52f4
ShenMian <sms_school@outlook.com>
2023-02-20 11:31:56 +0800

    .

--- src/engine/ecs/view.hpp
+++ src/engine/ecs/view.hpp
@@ -49,13 +49,47 @@ public:
 		Manager&                      manager_;
 	};
 
+	class ConstIterator
+	{
+	public:
+		using iterator_category = std::forward_iterator_tag;
+		using value_type        = std::tuple<const Entity&, const Ts&...>;
+
+		ConstIterator(std::vector<Entity>::iterator it, Manager& mgr) : iterator_(it), manager_(mgr){};
+
+		value_type operator*()
+		{
+			return std::tuple_cat(std::make_tuple(*iterator_), manager_.get_components<Ts...>(*iterator_));
+		}
+
+		ConstIterator& operator++() noexcept
+		{
+			iterator_++;
+			return *this;
+		}
+
+		ConstIterator operator++(int) noexcept
+		{
+			Iterator it = *this;
+			++(*this);
+			return it;
+		}
+
+		bool operator==(const ConstIterator& rhs) const noexcept { return iterator_ == rhs.iterator_; };
+		bool operator!=(const ConstIterator& rhs) const noexcept { return iterator_ != rhs.iterator_; };
+
+	private:
+		mutable std::vector<Entity>::iterator iterator_;
+		Manager&                              manager_;
+	};
+
 	View(std::vector<Entity>& entities, Manager& manager) : entities_(entities), manager_(manager) {}
 
 	Iterator begin() noexcept { return {entities_.begin(), manager_}; };
 	Iterator end() noexcept { return {entities_.end(), manager_}; };
 
-	const Iterator begin() const noexcept { return {entities_.begin(), manager_}; };
-	const Iterator end() const noexcept { return {entities_.end(), manager_}; };
+	ConstIterator begin() const noexcept { return {entities_.begin(), manager_}; };
+	ConstIterator end() const noexcept { return {entities_.end(), manager_}; };
 
 private:
 	std::vector<Entity>& entities_;
