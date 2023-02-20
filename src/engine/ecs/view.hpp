1c182ee305de114fb907e7a44500c2a5ce64cdd2
ShenMian <sms_school@outlook.com>
2023-02-20 13:31:59 +0800

    .

--- src/engine/ecs/view.hpp
+++ src/engine/ecs/view.hpp
@@ -12,90 +12,89 @@ namespace ecs
 {
 
 template <typename... Ts>
+class ViewIterator
+{
+public:
+	using iterator_category = std::forward_iterator_tag;
+	using value_type        = std::tuple<const Entity&, Ts&...>;
+
+	ViewIterator(std::vector<Entity>::iterator it, Manager& mgr) : iterator_(it), manager_(mgr) {}
+
+	std::tuple<const Entity&, Ts&...> operator*()
+	{
+		return std::tuple_cat(std::make_tuple(*iterator_), manager_.get_components<Ts...>(*iterator_));
+	}
+
+	ViewIterator& operator++() noexcept
+	{
+		iterator_++;
+		return *this;
+	}
+
+	ViewIterator operator++(int) noexcept
+	{
+		ViewIterator it = *this;
+		++(*this);
+		return it;
+	}
+
+	bool operator==(const ViewIterator& rhs) const noexcept { return iterator_ == rhs.iterator_; };
+	bool operator!=(const ViewIterator& rhs) const noexcept { return iterator_ != rhs.iterator_; };
+
+private:
+	std::vector<Entity>::iterator iterator_;
+	Manager&                      manager_;
+};
+
+template <typename... Ts>
+class ViewConstIterator
+{
+public:
+	using iterator_category = std::forward_iterator_tag;
+	using value_type        = std::tuple<const Entity&, const Ts&...>;
+
+	ViewConstIterator(std::vector<Entity>::iterator it, Manager& mgr) : iterator_(it), manager_(mgr){};
+
+	value_type operator*()
+	{
+		return std::tuple_cat(std::make_tuple(*iterator_), manager_.get_components<Ts...>(*iterator_));
+	}
+
+	ViewConstIterator& operator++() noexcept
+	{
+		iterator_++;
+		return *this;
+	}
+
+	ViewConstIterator operator++(int) noexcept
+	{
+		ViewIterator it = *this;
+		++(*this);
+		return it;
+	}
+
+	bool operator==(const ViewConstIterator& rhs) const noexcept { return iterator_ == rhs.iterator_; };
+	bool operator!=(const ViewConstIterator& rhs) const noexcept { return iterator_ != rhs.iterator_; };
+
+private:
+	std::vector<Entity>::iterator iterator_;
+	Manager&                      manager_;
+};
+
+template <typename... Ts>
 class View
 {
 public:
-	class Iterator
-	{
-	public:
-		using iterator_category = std::forward_iterator_tag;
-		using value_type        = std::tuple<const Entity&, Ts&...>;
-
-		Iterator(std::vector<Entity>::iterator it, Manager& mgr) : iterator_(it), manager_(mgr) {}
-
-		std::tuple<const Entity&, Ts&...> operator*()
-		{
-			return std::tuple_cat(std::make_tuple(*iterator_), manager_.get_components<Ts...>(*iterator_));
-		}
-
-		Iterator& operator++() noexcept
-		{
-			iterator_++;
-			return *this;
-		}
-
-		Iterator operator++(int) noexcept
-		{
-			Iterator it = *this;
-			++(*this);
-			return it;
-		}
-
-		bool operator==(const Iterator& rhs) const noexcept { return iterator_ == rhs.iterator_; };
-		bool operator!=(const Iterator& rhs) const noexcept { return iterator_ != rhs.iterator_; };
-
-	private:
-		std::vector<Entity>::iterator iterator_;
-		Manager&                      manager_;
-	};
-
-	class ConstIterator
-	{
-	public:
-		using iterator_category = std::forward_iterator_tag;
-		using value_type        = std::tuple<const Entity&, const Ts&...>;
-
-		ConstIterator(std::vector<Entity>::iterator it, Manager& mgr) : iterator_(it), manager_(mgr){};
-
-		value_type operator*()
-		{
-			return std::tuple_cat(std::make_tuple(*iterator_), manager_.get_components<Ts...>(*iterator_));
-		}
-
-		ConstIterator& operator++() noexcept
-		{
-			iterator_++;
-			return *this;
-		}
-
-		ConstIterator operator++(int) noexcept
-		{
-			Iterator it = *this;
-			++(*this);
-			return it;
-		}
-
-		bool operator==(const ConstIterator& rhs) const noexcept { return iterator_ == rhs.iterator_; };
-		bool operator!=(const ConstIterator& rhs) const noexcept { return iterator_ != rhs.iterator_; };
-
-	private:
-		std::vector<Entity>::iterator iterator_;
-		Manager&                      manager_;
-	};
-
-	using iterator       = Iterator;
-	using const_iterator = ConstIterator;
+	using iterator       = ViewIterator<Ts...>;
+	using const_iterator = ViewConstIterator<Ts...>;
 
 	View(std::vector<Entity>& entities, Manager& manager) : entities_(entities), manager_(manager) {}
 
 	iterator begin() noexcept { return {entities_.begin(), manager_}; };
 	iterator end() noexcept { return {entities_.end(), manager_}; };
 
-	iterator begin() const noexcept { return {entities_.begin(), manager_}; };
-	iterator end() const noexcept { return {entities_.end(), manager_}; };
-
-	const_iterator cbegin() const noexcept { return {entities_.begin(), manager_}; };
-	const_iterator cend() const noexcept { return {entities_.end(), manager_}; };
+	const_iterator begin() const noexcept { return {entities_.begin(), manager_}; };
+	const_iterator end() const noexcept { return {entities_.end(), manager_}; };
 
 private:
 	std::vector<Entity>& entities_;
